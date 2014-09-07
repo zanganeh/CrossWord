9f68d00c4d3fb758def777c650b5f93751e0012c
zanganeh <zanganeh@gmail.com>
2014-09-07 17:32:46 +0430

    Import First

---
+++ development/android/assets/forge/app_config.js
@@ -0,0 +1,55 @@
+window.forge = {}; window.forge.config = {
+    "author": "zanganeh@gmail.com", 
+    "config_hash": "c836bf4ea53e8e602c832e35cc5df1c4816c1941", 
+    "config_version": "4", 
+    "core": {
+        "android": {
+            "package_name": "io.trigger.forge8a4cfca035b611e4b9df12313b0234c0"
+        }, 
+        "firefox": {
+            "package_name": "8a4cfca035b611e4b9df12313b0234c0"
+        }, 
+        "general": {
+            "logging": {
+                "level": "DEBUG"
+            }, 
+            "reload": true
+        }, 
+        "ie": {
+            "package_name": "{EE135490-899E-7265-E1B3-3B647F4B7BEC}"
+        }, 
+        "ios": {
+            "package_name": "io.trigger.forge8a4cfca035b611e4b9df12313b0234c0"
+        }, 
+        "osx": {
+            "package_name": "io.trigger.forge8a4cfca035b611e4b9df12313b0234c0"
+        }, 
+        "safari": {
+            "package_name": "forge.safari.puzzle8a4cfca035b611e4b9df12313b0234c0"
+        }
+    }, 
+    "description": "An empty app created by default", 
+    "modules": {
+        "prefs": {
+            "version": "2.0"
+        }, 
+        "request": {
+            "config": {
+                "permissions": []
+            }, 
+            "version": "2.5"
+        }, 
+        "topbar": {
+            "version": "2.4"
+        }
+    }, 
+    "name": "Puzzle", 
+    "platform_version": "v2.1.9", 
+    "trigger_domain": "https://trigger.io", 
+    "uuid": "8a4cfca035b611e4b9df12313b0234c0", 
+    "version": "0.1"
+}; window.forge.module_mapping = {
+    "prefs": "prefs", 
+    "request": "request", 
+    "topbar": "topbar"
+};
